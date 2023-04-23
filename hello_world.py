e8f552b479c3ce5345e32165dcde4c7e71d9b37f
Georgian Ghelase <ghelase.georgian@gmail.com>
2023-04-23 23:54:49 +0100

    Task 1

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# Compulsory Task 1
+# Completed after reading the example.py, watch the video Your First Computer Program, and Using Variables
+# and attending "Skills Bootcamp Cohort 4"
+
+user_name = input('Please insert your name: ')
+print(user_name)
+
+user_age = input('Please enter your age: ')
+print(user_age)
+
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,10 @@
+# Compulsory Task 2
+# Completed after reading the example.py, watch the video Your First Computer Program, and Using Variables
+# and attending "Skills Bootcamp Cohort 4"
+
+print('Please order your 3 favourite food items')
+item1 = input("")
+item2 = input("")
+item3 = input("")
+print(" ") #used it to create an empty line
+print('Order confirmation! You have ordered:\n{}\n{}\n{}'.format(item1,item2,item3))
