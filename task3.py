e7d33ef87902b40ab731b88aaab4ce24d712c3b4
Georgian Ghelase <ghelase.georgian@gmail.com>
2023-04-23 23:57:41 +0100

    Task 8

--- task3.py
+++ task3.py
@@ -1,28 +1,16 @@
-# create a program that determines the award a person competing in a triathlon will receive.
+#  Create a loop that will produce the following output:
+""" *
+    **
+    ***
+    ****
+    ***** """
 
-# we ask the user to insert the times(in minutes) for all three events of a triathlon
-swimming = int(input('Please enter the time(in minutes) taken to complete the swimming event: '))
-cycling = int(input('Please enter the time(in minutes) taken to complete the cycling event:  '))
-running = int(input('Please enter the time(in minutes) taken to complete the running event:  '))
 
-# we now store them in a tuple and print the total output
-total_triathlon = (swimming,cycling,running) 
-print(f"\nYou're total time to complete the triathlon is: {sum(total_triathlon)}.")
+# create an empty string
+star = " " 
 
-# following if statements will display the award a participant receives by using comparison and logical operators
-
-# total time within qualifying time (100)
-if sum(total_triathlon) <= 100:
-    print("\nCongratulations! You've been awarded \"Provincial Colors\".")
-
-# within 5 minutes of qualifying time
-elif sum(total_triathlon) > 100 and sum(total_triathlon) <= 105:
-    print("\nCongratulations! You've been awarded \"Provincial Half Colors\".")
-
-# within 10 minutes of qualifying time
-elif sum(total_triathlon) > 105 and sum(total_triathlon) <= 110:
-    print("\nCongratulations! You've been awarded \"Provincial Scroll\".")
-
-# more than 10 minutes of qualifying time
-else:
-    print("\nWe're sorry, you will not receive a reward.")
+# we iterate through the for loop 5 times (5*)
+for i in range(5): 
+    # we add 1 star at a time
+    star += "*"
+    print(star)
